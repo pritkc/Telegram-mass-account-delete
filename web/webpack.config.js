acdf888dfeeffe2b492c129fb773d0bc564e65d6
Prit Chakalasiya <prit.chakalasiya@gmail.com>
2026-08-13 19:04:02 -0500

    Configure browser bundle

---
+++ web/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+    clean: true,
+  },
+  target: "web",
+  resolve: {
+    extensions: [".js"],
+  },
+  performance: {
+    hints: false,
+  },
+};
